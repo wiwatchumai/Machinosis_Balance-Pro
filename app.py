b29bc7bb568b5f0044bdf7782a8f48bb69f44cfc
Wiwat Chumai <wiwatchumai@gmail.com>
2025-06-28 00:27:34 +0700

    write more space

--- app.py
+++ app.py
@@ -110,6 +110,7 @@ if st.button("Run Balancing Analysis"):
     st.title("Author")
     col1, col2 = st.columns([2, 8])
     with col1:
+        st.write("")  # Add vertical space above the image
         st.image("author.png", width=120)  #
     with col2:
         st.write("Wiwat Chumai")  # Add vertical space above the image
